4d8177640d284fc22b3e0067e8bc23ec12712ab2
shereef <shereefmhmdk@gmail.com>
2024-12-06 09:16:05 +0530

    first

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Event;
+use Illuminate\Http\Request;
+
+class EventController extends Controller
+{
+    public function index()
+    {
+        return response()->json(Event::all());
+    }
+
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => 'required|string|max:255',
+            'start' => 'required|date',
+            'end' => 'nullable|date|after_or_equal:start',
+        ]);
+
+        $event = Event::create($validated);
+
+        return response()->json($event, 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $event = Event::findOrFail($id);
+
+        $validated = $request->validate([
+            'title' => 'required|string|max:255',
+        ]);
+
+        $event->update($validated);
+
+        return response()->json($event, 200);
+    }
+
+    public function destroy($id)
+    {
+        $event = Event::findOrFail($id);
+        $event->delete();
+
+        return response()->json(['message' => 'Event deleted successfully'], 200);
+    }
+}
